702e2962da55b16d9592e120dd9cf5da67785c98
guannan-he <50306947+guannan-he@users.noreply.github.com>
2020-08-06 11:04:51 +0800

    Update main_prog.m
    add speed profile generation function

--- TUM_minimum_curvature/main_prog.m
+++ TUM_minimum_curvature/main_prog.m
@@ -2,79 +2,106 @@ clear;
 clc;
 load 'track_352.mat';%原始赛道信息
 load 'body.mat';%车辆信息
+load 'dynamic_constrains.mat';%动力学约束
 safe_dist = 2;%距离障碍物安全距离
 %% 平滑插值赛道
-tolerance = 5;%先线性插值，去掉大间隙，步长不要大于下一步的二倍（经验）
+tolerance = 10;%先线性插值，去掉大间隙
 [track_x,track_y] = track_smooth_linear(track_x,track_y,tolerance);
 tolerance = 3;%然后三次样条线插值，达到最终步长
 [track_x,track_y] = track_smooth_spline(track_x,track_y,tolerance);
+clear tolerance;
 
 %% 求得与赛道长度对应的样条线参数
 [c,~] = size(track_x);
 [spline_A] = spline_matrix_gen(c);%样条线求解矩阵，要留在内存中，加速计算
-para_x = spline_A * track_x;
-para_y = spline_A * track_y;
+para_x = spline_A * track_x;%x三次样条参数
+para_y = spline_A * track_y;%y三次样条参数
 
 %% 单位切向量
 [vector] = vector_gen(para_x,para_y,c);
 
 %% 绘制中心线、左右边界及障碍物
 draw_track(track_x,track_y,vector,track_w,obs_x,obs_y,c);
-clear x_l y_l x_r y_r;
 
 %% QP优化初始化
-alpha_out = zeros(c,1);
-alpha_last = zeros(c,1);
-[lb,ub] = init_lub(track_w,body.Wb,c);
-[Aieq,bieq] = init_ieq(track_x,track_y,obs_x,obs_y,safe_dist,c);
+alpha_out = zeros(c,1);%本次次迭代结果
+alpha_last = zeros(c,1);%上一次迭代结果
+[lb,ub] = init_lub(track_w,body.Wb,c);%上下界初始化
+[Aieq,bieq] = init_ieq(track_x,track_y,obs_x,obs_y,safe_dist,c);%有关障碍物的不等式约束初始化
+stop_inter_thres = c * 0.01;%每一次迭代后所有点上横向改变平方和，每一个点上差别不超过1厘米
+inter_val = 100000;%初始停止条件数值
+cnt = 0;%迭代次数
 
 %% QP优化
-[alpha_out,alpha_last,lb,ub,track_x,track_y,vector] = QP_optimization(spline_A,alpha_out,alpha_last,lb,ub,Aieq,bieq,track_x,track_y,vector,c);
-draw_optimised_path(track_x,track_y,vector,alpha_out,'m');
-[alpha_out,alpha_last,lb,ub,track_x,track_y,vector] = QP_optimization(spline_A,alpha_out,alpha_last,lb,ub,Aieq,bieq,track_x,track_y,vector,c);
-draw_optimised_path(track_x,track_y,vector,alpha_out,'k');
-[alpha_out,alpha_last,lb,ub,track_x,track_y,vector] = QP_optimization(spline_A,alpha_out,alpha_last,lb,ub,Aieq,bieq,track_x,track_y,vector,c);
-draw_optimised_path(track_x,track_y,vector,alpha_out,'c');
+%不下降时停止迭代
+tic;
+while inter_val >= stop_inter_thres
+    [alpha_out,alpha_last,lb,ub,track_x,track_y,vector] = QP_optimization(spline_A,alpha_out,alpha_last,lb,ub,Aieq,bieq,track_x,track_y,vector,c);
+    inter_val = sum(alpha_out.^2);
+    cnt = cnt + 1;
+end
+[c_alpha,~] = size(alpha_out);
+if c_alpha == 0
+    alpha_out = zeros(c,1);%以防无解
+end
+clear c_alpha;
+t = toc;
+clc;
+fprintf('迭代%d次，耗时%f秒\n',cnt,t);
+clear stop_inter_thres inter_val t lb ub Aieq bieq cnt;
 
-%% 绘制单位向量
-% figure(2)
-% clf;
-% plot(vector(:,1));
-% hold on;
-% plot(vector(:,2));
+%% 生成最优路径及其信息
+draw_optimised_path(track_x,track_y,vector,alpha_out,'m');%绘制最优路径
+[track_x,track_y] = path_gen(track_x,track_y,vector,alpha_out);%保存最优路径
+[distance] = path_distance_calculate(track_x,track_y,c);%沿最优路径距离
+para_x = spline_A * track_x;
+para_y = spline_A * track_y;
+[vector] = vector_gen(para_x,para_y,c);
+phi = atan(vector(:,2) ./ vector(:,1));%参考车辆航向角
+[curvature_res] = get_curvature(para_x,para_y,c);%参考曲率
+clear obs_x obs_y obs_w safe_dist alpha_out alpha_last;
 
+%% 速度规划
+[apex_location,apex_cnt] = get_apex(curvature_res,c);%标记Apex点
+vmax = 40;%限制最高直线车速
+[vel_geo] = geometry_vel_calculate(curvature_res,vmax,ay_para,c);%计算几何速度限制
+[vel_forward] = forward_calculate(apex_location,apex_cnt,vel_geo,curvature_res,distance,acc_para,ay_para,c);%加速限制
+[vel_backward] = backward_calculate(apex_location,apex_cnt,vel_geo,curvature_res,distance,brk_para,ay_para,c);%减速限制
+vel_profile = min(vel_geo,min(vel_forward,vel_backward));%三种取最小值
+clear apex_location apex_cnt vel_geo vel_forward vel_backward acc_para ay_para brk_para;
 
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%子函数%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+figure(2);
+clf;
+plot(distance,vel_profile);%绘制速度-距离规划
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%路径规划子函数%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% 生成样条线求解矩阵
 function [spline_A] = spline_matrix_gen(c) %[a;b;c;d] = A * y
-%% 输入D
+%%%%%%%% 输入D
 D = 3 * [eye(c - 2),zeros(c - 2,2)] -6 * [zeros(c - 2,1),eye(c - 2),zeros(c - 2,1)] + 3 * [zeros(c - 2,2),eye(c - 2)];
 tmp = [-3,3,zeros(1,c - 4),3,-3];
 D = [tmp;D;tmp];
-%% c
+%%%%%%%% c
 A = eye(c) + 0.5 * [zeros(c - 1,1),eye(c - 1);zeros(1,c)];
 A = A + A';
 A(1,c - 1) = 0.5;
 A(c,2) = 0.5;
 matrix_c = 2 * A \ D;
-%% a
+%%%%%%%% a
 matrix_a = eye(c);
-%% b
+%%%%%%%% b
 Ab = eye(c) / 6 + [zeros(c - 1,1),eye(c - 1) / 3;zeros(1,c)];
 Ab(c,2) = 1 / 3;
 Aby = [-eye(c - 1),zeros(c - 1,1)] + [zeros(c - 1,1),eye(c - 1)];
 Aby = [Aby;Aby(1,:)];
 matrix_b = Ab / A * D + Aby;
-%% d
+%%%%%%%% d
 Ad = eye(c) / 6 - [zeros(1,c);[eye(c - 1) / 6,zeros(c - 1,1)]];
 Ad(1,c - 1) = -1 / 6;
 matrix_d = Ad / A * D;
-%% 整合
+%%%%%%% 整合
 zero = zeros(c);
 A_sig = [matrix_a,zero,zero,zero
     zero,matrix_b,zero,zero
@@ -279,13 +306,13 @@ for i = 1:c - 1
         for k = 1:n
             val = (k - 1) / n;
             temp_x(k) = Mx(i) * (1 - val)^3 / 6 ...
-                      + Mx(i + 1) * (val - 0)^3 / 6 ...
-                      + (track_x(i) - Mx(i) / 6) * (1 - val) ...
-                      + (track_x(i + 1) - Mx(i + 1) / 6) * (val - 0);
+                + Mx(i + 1) * (val - 0)^3 / 6 ...
+                + (track_x(i) - Mx(i) / 6) * (1 - val) ...
+                + (track_x(i + 1) - Mx(i + 1) / 6) * (val - 0);
             temp_y(k) = My(i) * (1 - val)^3 / 6 ...
-                      + My(i + 1) * (val - 0)^3 / 6 ...
-                      + (track_y(i) - My(i) / 6) * (1 - val) ...
-                      + (track_y(i + 1) - My(i + 1) / 6) * (val - 0);
+                + My(i + 1) * (val - 0)^3 / 6 ...
+                + (track_y(i) - My(i) / 6) * (1 - val) ...
+                + (track_y(i + 1) - My(i + 1) / 6) * (val - 0);
         end
         x_out = [x_out;temp_x];
         y_out = [y_out;temp_y];
@@ -314,6 +341,109 @@ alpha_out = quadprog(H,F,Aieq,bieq,Aeq,beq,lb,ub);%(这次优化后的坐标)
 end
 
 %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%速度规划子函数%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 计算路径长度
+function [distance] = path_distance_calculate(track_x,track_y,c)
+distance = zeros(c,1);
+for i = 2:c
+    distance(i) = sqrt((track_x(i) - track_x(i - 1))^2 + (track_y(i) - track_y(i - 1))^2) + distance(i - 1);
+end
+end
+
+%% 获取曲率
+function [curvature] = get_curvature(para_x,para_y,c)
+select_b = [zeros(c),eye(c),zeros(c),zeros(c)];
+select_c = [zeros(c),zeros(c),eye(c),zeros(c)];
+Dx = select_b * para_x;
+Dy = select_b * para_y;
+DDx = 2 * select_c * para_x;
+DDy = 2 * select_c * para_y;
+base = (Dx.^2 + Dy.^2).^1.5;
+curvature = (Dx .* DDy - Dy .* DDx) ./ base;
+end
+
+%% 标记Apex点
+function [apex_location,apex_cnt] = get_apex(curvature_res,c)
+apex = zeros(c,1);
+apex(1) = 1;
+apex(c) = 1;
+for i = 2:c - 1%标记Apex点
+    if ((curvature_res(i + 1) - curvature_res(i)) * (curvature_res(i) - curvature_res(i - 1)) <= 0)% && (sum(apex(i - 4:i)) == 0)
+        apex(i) = 1;
+    end
+end
+apex_location = find(apex);
+[apex_cnt,~] = size(apex_location);
+end
+
+%% 卡尔曼滤波
+function [curvature_res] = smooth_curvature_kalman(curvature,c,Q,R)
+curvature_res = zeros(c,1);
+x = curvature(1);
+P = 100;
+% curvature_res(1) = x;
+for i = 1:c
+    xnp = x;
+    Pnp = P + Q;
+    G = Pnp / (Pnp + R);
+    x = xnp + G * (curvature(i) - xnp);
+    P = (1 - G) * Pnp;
+    curvature_res(i) = x;
+end
+end
+
+%% 几何速度(查表法)
+function [vel_geo] = geometry_vel_calculate(curvature_res,vmax,ay_para,c)
+vel_geo = zeros(c,1);
+vel_ran = [5:0.001:40]';
+k_v = polyval(ay_para,vel_ran) ./ vel_ran.^2;
+[kv_num,~] = size(k_v);
+for i = 1:c
+    j = kv_num;
+    while abs(curvature_res(i)) >= k_v(j)
+        j = j - 1;
+    end
+    vel_geo(i) = min(vel_ran(j - 1),vmax);
+end
+end
+
+%% 前向计算加速度
+function [vel_forward] = forward_calculate(apex_location,apex_cnt,vel_geo,curvature,distance,acc_para,ay_para,c)
+vel_forward = zeros(c,1);
+vel_forward(1) = vel_geo(1);
+for i = 1:apex_cnt - 1
+    v_ref = min(vel_geo(apex_location(i)),vel_forward(apex_location(i)));%Apex点参考速度和上一步得出速度最小值
+    for j = apex_location(i) + 1:apex_location(i + 1)
+        ax_max = polyval(acc_para,v_ref);
+        ay_max = polyval(ay_para,v_ref);
+        delta_s = distance(j) - distance(j - 1);
+        if ((v_ref^2 * curvature(j - 1)) / (ay_max))^2 < 1%确保车辆不失稳（安全域）
+            v_ref = sqrt(v_ref^2 + 2 * delta_s * ax_max * sqrt(1 - ((v_ref^2 * curvature(j - 1)) / (ay_max))^2));
+        end
+        vel_forward(j) = v_ref;
+    end
+end
+vel_forward(c) = v_ref;
+end
+
+%% 反向计算减速度
+function [vel_backward] = backward_calculate(apex_location,apex_cnt,vel_geo,curvature,distance,brk_para,ay_para,c)
+vel_backward = zeros(c,1);
+vel_backward(c) = vel_geo(c);
+for i = apex_cnt + 1 - [1:apex_cnt - 1]
+    v_ref = min(vel_geo(apex_location(i)),vel_backward(apex_location(i)));%Apex点参考速度和上一段制动结束速度最小值
+    for j = apex_location(i) - [1:apex_location(i) - apex_location(i - 1)]
+        ax_max = polyval(brk_para,v_ref);
+        ay_max = polyval(ay_para,v_ref);
+        delta_s = distance(j + 1) - distance(j);
+        if ((v_ref^2 * curvature(j + 1)) / (ay_max))^2 < 1%确保车辆不失稳（安全域）
+            v_ref = sqrt(v_ref^2 + 2 * delta_s * ax_max * sqrt(1 - ((v_ref^2 * curvature((j + 1)) / (ay_max))^2)));
+        end
+        vel_backward(j) = v_ref;
+    end
+end
+end
+
 %%
 %%
 %%
